97d34a02d46ed383e12af44023943a2353a11f33
yoyopro28 <joe.martens28@gmail.com>
2026-07-16 17:07:01 +0200

    Konfiguriere Cloudflare Worker

--- public/config.js
+++ public/config.js
@@ -1,7 +1,8 @@
 window.GROUPHUB_CONFIG = {
-  apiBase: "https://DEIN-WORKER.workers.dev",
+  apiBase: "https://landschaften-dateiablage-api.joe-martens28.workers.dev",
   repository: "yoyopro28/Landschaften-nachhaltig-nutzen-und-managen",
   branch: "main",
   rootPath: "workspace",
   maxUploadMb: 20
 };
+
